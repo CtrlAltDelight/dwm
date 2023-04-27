babb0d6f29a028ceea7a5bd8585d521b55c89fb3
CtrlAltDelight <lbchigges@gmail.com>
2023-04-27 14:44:03 -0400

    Added volume keys and rofi ssh

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 1;  /* border pixel of windows */
+static const unsigned int borderpx = 2;  /* border pixel of windows */
 static const unsigned int gappx    = 10; /* gap pixel between windows */
 static const unsigned int snap     = 32; /* snap pixel */
 static const int showbar           = 1;  /* 0 means no bar */
 static const int topbar            = 1;  /* 0 means bottom bar */
-static const char *fonts[]         = { "Fira Code:size=10" };
+static const char *fonts[]         = { "Fira Code:size=12" };
 static const char dmenufont[]      = "Fira Code:size=10";
 static const char col_gray1[]      = "#222222";
 static const char col_gray2[]      = "#444444";
@@ -64,9 +64,11 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "/bin/kitty", NULL };
 
+#include <X11/XF86keysym.h>
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          SHCMD("rofi -show run") },
+	{ MODKEY,                       XK_o,      spawn,          SHCMD("rofi -show ssh") },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -89,6 +91,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ 0,                            XF86XK_AudioLowerVolume, spawn, SHCMD("pulsemixer --change-volume -5")},
+	{ 0,                            XF86XK_AudioRaiseVolume, spawn, SHCMD("pulsemixer --change-volume +5")},
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
